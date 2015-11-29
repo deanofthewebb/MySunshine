5fe7491ebd3c09f9c3cf842797eb461e8f7851aa
Jay Jun <jayjun@Jayui-MacBook-Pro.local>
2015-11-29 23:15:26 +0900

    Implement share action

--- app/src/main/java/com/example/jayjun/sunshine/DetailActivity.java
+++ app/src/main/java/com/example/jayjun/sunshine/DetailActivity.java
@@ -31,12 +31,17 @@ public class DetailActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_detail);
+        //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        //setSupportActionBar(toolbar);
+
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
                     .add(R.id.container, new PlaceholderFragment())
                     .commit();
         }
+
     }
 
 
@@ -46,6 +51,7 @@ public class DetailActivity extends ActionBarActivity {
         getMenuInflater().inflate(R.menu.detail, menu);
         return true;
     }
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
--- app/src/main/java/com/example/jayjun/sunshine/DetailActivityFragment.java
+++ app/src/main/java/com/example/jayjun/sunshine/DetailActivityFragment.java
@@ -3,7 +3,13 @@ package com.example.jayjun.sunshine;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.MenuItemCompat;
+import android.support.v7.widget.ShareActionProvider;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -13,7 +19,42 @@ import android.widget.TextView;
  */
 public class DetailActivityFragment extends Fragment {
 
+    private static final String LOG_TAG = DetailActivityFragment.class.getSimpleName();
+    private static final String FORECAST_SHARE_HASHTAG = " #SunshineApp";
+    private String mForecastStr;
+/*
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        ActionBar ab = (ActionBar) getActivity().getActionBar();
+        if(ab != null) {
+            ab.setTitle("Detail Sunshine");
+        }
+    }
+*/
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Add this line in order for this fragment to handle menu events.
+        setHasOptionsMenu(true);
+    }
+
     public DetailActivityFragment() {
+      //  setHasOptionsMenu(true);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.detailfragment, menu);
+        MenuItem menuItem = menu.findItem(R.id.action_share);
+        ShareActionProvider mShareActionProvider = (ShareActionProvider) MenuItemCompat.getActionProvider(menuItem);
+
+        if(mShareActionProvider != null) {
+            mShareActionProvider.setShareIntent(createShareForecastIntent());
+        }else{
+            Log.d(LOG_TAG, "Share Action Provider is null?");
+        }
+
     }
 
     @Override
@@ -22,15 +63,23 @@ public class DetailActivityFragment extends Fragment {
 
         View fragmentView = inflater.inflate(R.layout.fragment_detail, container, false);
 
-        TextView weatherText = (TextView) fragmentView.findViewById(R.id.weather_text);
+        //TextView weatherText = (TextView) fragmentView.findViewById(R.id.weather_text);
 
         Intent intent = getActivity().getIntent();
-        String txt = intent.getStringExtra(Intent.EXTRA_TEXT);
+        if(intent != null &&  intent.hasExtra(Intent.EXTRA_TEXT)) {
+            mForecastStr = intent.getStringExtra(Intent.EXTRA_TEXT);
+            ((TextView) fragmentView.findViewById(R.id.weather_text)).setText(mForecastStr);
 
-
-
-        weatherText.setText(txt);
+        }
 
         return fragmentView;
+    }
+
+    private Intent createShareForecastIntent() {
+        Intent shareIntent = new Intent(Intent.ACTION_SEND);
+        shareIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET);
+        shareIntent.setType("text/plain");
+        shareIntent.putExtra(Intent.EXTRA_TEXT, mForecastStr + FORECAST_SHARE_HASHTAG );
+        return  shareIntent;
     }
 }
